9253c6313ab33f4543c22aec8f6a8ec453fdeae3
Arseniy Krupenin <arsenii@Arseniys-MacBook-Pro.local>
2020-05-14 00:27:59 +0300

    update 3

--- 3_Longest_Substring_Without_Repeating_Characters.playground/Contents.swift
+++ 3_Longest_Substring_Without_Repeating_Characters.playground/Contents.swift
@@ -8,16 +8,21 @@
 import Foundation
 
 class Solution {
+
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        guard s.count > 0 else { return 0 }
-        var (maximum, temp) = (1, "")
-        for symbol in s {
-            if temp.contains(symbol) {
-                maximum = max(temp.count, maximum)
-                if let last = temp.components(separatedBy: String(symbol)).last { temp = last }
-            }
-            temp.append(symbol)
-        }
-        return max(temp.count, maximum)
+       if s.isEmpty { return 0 } else if s.count == 1 { return 1 }
+       let array = Array(s)
+       var length = 1
+       var temp = 0
+       let last = array.count - 1
+       for i in 1...last {
+           let symbol = array[i]
+           let slice = array[temp..<i]
+           if length < slice.count { length = slice.count }
+           if let index = slice.firstIndex(of: symbol) {
+               temp = index + 1
+           } else if i == last { length = max(array[temp...i].count, length) }
+       }
+       return length
     }
 }
